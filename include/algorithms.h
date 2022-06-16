cb5cb58e838721f7e21ec3531554dbb92ddedaf7
Bogdan Ciurea <ciureabogdanalexandru@yahoo.com>
2022-06-16 15:56:02 +0000

    Structure added

---
+++ include/algorithms.h
@@ -0,0 +1,8 @@
+#ifndef ALGORITHMS_HPP
+#define ALGORITHMS_HPP
+
+#include <iostream>
+
+void hello_world();
+
+#endif
---
+++ src/algorithms.cpp
@@ -0,0 +1,6 @@
+#include "algorithms.h"
+
+void hello_world()
+{
+    std::cout << "Hello world!\n";
+}
