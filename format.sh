5918bc83f4f82f0dad33c9052f6c93ba02505833
Bogdan Ciurea <bogdanciurea@Bogdans-MacBook-Pro.local>
2022-08-09 23:36:41 +0300

    added additional files

---
+++ format.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+clang-format -i src/*.cpp
+clang-format -i include/*.h
+clang-format -i include/standalone-functions/*.h
--- include/standalone-functions/sorting-algorithms.h
+++ include/standalone-functions/sorting-algorithms.h
@@ -3,16 +3,21 @@
 
 #include "pillar.h"
 
-std::vector<std::vector<Pillar>> insertion_sort_algorithm (std::vector<Pillar> array);
+std::vector<std::vector<Pillar>> insertion_sort_algorithm(
+    std::vector<Pillar> array);
 
-std::vector<std::vector<Pillar>> heap_sort_algorithm (std::vector<Pillar> array);
+std::vector<std::vector<Pillar>> heap_sort_algorithm(std::vector<Pillar> array);
 
-std::vector<std::vector<Pillar>> selection_sort_algorithm (std::vector<Pillar> array);
+std::vector<std::vector<Pillar>> selection_sort_algorithm(
+    std::vector<Pillar> array);
 
-std::vector<std::vector<Pillar>> merge_sort_algorithm (std::vector<Pillar> array);
+std::vector<std::vector<Pillar>> merge_sort_algorithm(
+    std::vector<Pillar> array);
 
-std::vector<std::vector<Pillar>> quick_sort_algorithm (std::vector<Pillar> array);
+std::vector<std::vector<Pillar>> quick_sort_algorithm(
+    std::vector<Pillar> array);
 
-std::vector<std::vector<Pillar>> bubble_sort_algorithm (std::vector<Pillar> array);
+std::vector<std::vector<Pillar>> bubble_sort_algorithm(
+    std::vector<Pillar> array);
 
 #endif
